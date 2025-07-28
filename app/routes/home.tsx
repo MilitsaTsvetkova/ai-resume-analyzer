d86b907b19999a7d7e6cc7a87e13c1190064cfb8
MilitsaTsvetkova <militsa88@yahoo.com>
2025-07-28 10:02:32 +0300

    home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
+import { resumes } from "constants/data";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback fro your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
